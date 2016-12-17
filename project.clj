b50e37b74446eb4a6623ed18040ff8826d7fe62d
David Cook <divergentdave@gmail.com>
2016-12-17 14:03:31 -0600

    Update project metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject tenforty "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name ""
-            :url ""}
+  :description "Tools for analyzing U.S. taxes"
+  :url "https://github.com/divergentdave/tenforty"
+  :license {:name "GNU GPL v2"
+            :url "https://www.gnu.org/licenses/gpl-2.0.en.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
