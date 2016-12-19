84b05454ced0e6fbda20793f42626105d9213f8f
David Cook <divergentdave@gmail.com>
2016-12-18 23:17:22 -0600

    Refactor line dependencies test

--- test/tenforty/line_deps_test.clj
+++ test/tenforty/line_deps_test.clj
@@ -10,11 +10,11 @@
         contains (partial contains? universe)]
     (dorun (map #(is (contains %) (str %)) deps))))
 
-(let [universe (merge tenforty.forms.ty2015.f1040/form
-                      tenforty.forms.ty2015.s8812/form)]
-  (deftest f1040-deps
-    (testing "Check that lines are defined for all dependencies in form 1040"
-      (dorun (map (partial check-deps universe) (vals tenforty.forms.ty2015.f1040/form)))))
-  (deftest s8812-deps
-    (testing "Check that lines are defined for all dependencies in schedule 8812"
-      (dorun (map (partial check-deps universe) (vals tenforty.forms.ty2015.s8812/form))))))
+(let [forms {:f1040 tenforty.forms.ty2015.f1040/form
+             :s8812 tenforty.forms.ty2015.s8812/form}
+      universe (apply merge (vals forms))]
+  (deftest line-deps
+    (dorun (map #(testing (str "Check that lines are defined for all dependencies in " (key %))
+                   (dorun (map (partial check-deps universe)
+                               (vals (val %)))))
+                (seq forms)))))
