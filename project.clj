3413350f0272d5151452d1b04fbf815577d714d2
David Cook <divergentdave@gmail.com>
2016-12-28 12:15:48 -0600

    Add cljsbuild configuration

--- project.clj
+++ project.clj
@@ -5,4 +5,10 @@
             :url "https://www.gnu.org/licenses/gpl-2.0.en.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
+  :plugins [[lein-cljsbuild "1.1.5"]]
+  :cljsbuild {:builds [{:source-paths ["src"]
+                        :compiler {:output-dir "out"
+                                   :optimizations :none
+                                   :source-map true
+                                   :pretty-print true}}]}
   :profiles {:dev {:plugins [[lein-cljfmt "0.5.6"]]}})
