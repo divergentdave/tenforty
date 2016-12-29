0482b311332f642c7f59df4c534fc393a812f08a
David Cook <divergentdave@gmail.com>
2016-12-28 19:09:42 -0600

    Pass test status back out to PhantomJS

--- test/tenforty/test.cljs
+++ test/tenforty/test.cljs
@@ -4,5 +4,7 @@
 (enable-console-print!)
 
 (defn ^:export run
-  []
+  [callback]
+  (defmethod cljs.test/report [:cljs.test/default :end-run-tests] [m]
+    (callback (cljs.test/successful? m)))
   (run-all-tests #"tenforty.*-test"))
