3e0e88b0fe7029412f3df6e3380f2023793d61e1
David Cook <divergentdave@gmail.com>
2016-12-28 19:09:42 -0600

    Pass test status back out to PhantomJS

--- resources/test/test.js
+++ resources/test/test.js
@@ -6,7 +6,7 @@ phantom.onError = function(msg, trace) {
             msgStack.push(' -> ' + (t.file || t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function +')' : ''));
         });
     }
-    console.error(msgStack.join('\n'));
+    console.log(msgStack.join('\n'));
     phantom.exit(1);
 };
 
@@ -20,16 +20,34 @@ page.onError = function(msg, trace) {
             msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function +'")' : ''));
         });
     }
-    console.error(msgStack.join('\n'));
+    console.log(msgStack.join('\n'));
 };
 
 page.onConsoleMessage = function(msg) {
     console.log(msg);
 };
 
+function exit(code) {
+    if (page) {
+        page.close();
+    }
+    setTimeout(function() {
+        phantom.exit(code);
+    }, 0);
+}
+
+page.onCallback = function(successful) {
+    if (successful) {
+        exit(0);
+    } else {
+        exit(1);
+    }
+};
+
 page.open(require("system").args[1], function(status) {
     page.evaluate(function() {
-        tenforty.test.run();
+        tenforty.test.run(function(successful) {
+            window.callPhantom(successful);
+        });
     });
-    phantom.exit(0);
 });
